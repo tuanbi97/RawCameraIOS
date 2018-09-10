4585277771d486d1970050794bb43861d3dcb409
Tuan <tuan@wirelessprv-10-193-252-175.near.illinois.edu>
2018-09-10 16:51:54 -0500

    Initial Commit

---
+++ AVCam/PhotoCaptureDelegate.swift
@@ -0,0 +1,81 @@
+/*
+See LICENSE.txt for this sample’s licensing information.
+
+Abstract:
+Photo capture delegate.
+*/
+
+import AVFoundation
+import Photos
+
+class PhotoCaptureProcessor: NSObject {
+	private(set) var requestedPhotoSettings: AVCapturePhotoSettings
+	
+	private let completionHandler: (PhotoCaptureProcessor) -> Void
+	
+	private var photoData: Data?
+
+	init(with requestedPhotoSettings: AVCapturePhotoSettings,
+	     completionHandler: @escaping (PhotoCaptureProcessor) -> Void) {
+		self.requestedPhotoSettings = requestedPhotoSettings
+		self.completionHandler = completionHandler
+	}
+	
+	private func didFinish() {
+		completionHandler(self)
+	}
+    
+}
+
+extension PhotoCaptureProcessor: AVCapturePhotoCaptureDelegate {
+    /*
+     This extension includes all the delegate callbacks for AVCapturePhotoCaptureDelegate protocol
+    */
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, willBeginCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        if let error = error {
+            print("Error capturing photo: \(error)")
+        } else {
+            photoData = photo.fileDataRepresentation()
+        }
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
+        if let error = error {
+            print("Error capturing photo: \(error)")
+            didFinish()
+            return
+        }
+        
+        guard let photoData = photoData else {
+            print("No photo data resource")
+            didFinish()
+            return
+        }
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+                PHPhotoLibrary.shared().performChanges({
+                    let options = PHAssetResourceCreationOptions()
+                    let creationRequest = PHAssetCreationRequest.forAsset()
+                    options.uniformTypeIdentifier = self.requestedPhotoSettings.processedFileType.map { $0.rawValue }
+                    creationRequest.addResource(with: .photo, data: photoData, options: options)
+                    
+                    }, completionHandler: { _, error in
+                        if let error = error {
+                            print("Error occurered while saving photo to photo library: \(error)")
+                        }
+                        
+                        self.didFinish()
+                    }
+                )
+            } else {
+                self.didFinish()
+            }
+        }
+    }
+}
